1c178cd6b483066ced3be066152ec980d4ab45cb
howardhuhu <howardhuhu@163.com>
2018-03-20 15:04:42 +0800

    通过Unsafa和ByteBuffer实现的告诉序列化/反序列化功能代码示例

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,17 +1,37 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import sun.misc.Unsafe;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.lang.reflect.Field;
 
 /**
- * Created by zhoujing on 2015/8/30.
+ * Created by zhuhuapeng on 18-3-6.
  */
 public class TestMain {
 
-    public static final Logger logger = LoggerFactory.getLogger(TestMain.class);
+    public static void main(String[] args) throws  Exception{
+        Guard guard = new Guard();
+        System.out.println(  guard.giveAccess());
 
-    public static void main(String[] args) {
-        logger.info("test only for slf4j + log4j ");
+
+        Unsafe unsafe = getUnsafe();
+        Field f = guard.getClass().getDeclaredField("ACCESS_ALLOWED");
+        unsafe.putInt(guard, unsafe.objectFieldOffset(f), 42); // memory corruption
+        System.out.println(  guard.giveAccess());
     }
+
+    private static Unsafe getUnsafe() throws Exception
+    {
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        return (Unsafe) field.get(null);
+    }
+
+
+   static class Guard {
+        private int ACCESS_ALLOWED = 1;
+
+        public boolean giveAccess() {
+            return 42 == ACCESS_ALLOWED;
+        }
+    }
+
 }
---
+++ src/main/java/zhuhp/learn/perf/serializable/Test.java
@@ -0,0 +1,7 @@
+package zhuhp.learn.perf.serializable;
+
+/**
+ * Created by zhuhuapeng on 18-3-19.
+ */
+public class Test {
+}
